6770062c5e9254447b8f10f3abb73b188dc4ddd4
Costin Catalin <56517046+iamcatalinnn@users.noreply.github.com>
2020-02-26 18:20:14 +0200

    Add filter possibility
    Update leads.sql to incorporate act as function.

--- leads.sql
+++ leads.sql
@@ -17,7 +17,9 @@ WHEN lead.STATUS = 'CONVERTED' THEN 'Converted'
 ELSE 'Error' END as Category
 
 FROM catalincostin.FY20_LEADS lead
-WHERE lead.CAMPAIGN_SOURCE_CODE = :P3_CODE AND RETIRE_REASON NOT LIKE 'Z-ADMIN: Admin Only%')
+LEFT JOIN FLM_SALES S on S.REP = lead.TERRITORY_OWNER_E_MAIL
+WHERE lead.CAMPAIGN_SOURCE_CODE = :P3_CODE AND RETIRE_REASON NOT LIKE 'Z-ADMIN: Admin Only%' 
+AND (:P1_VP = S.VP OR :P1_VP = '<All>') AND (:P1_DIR = S.DIR OR :P1_DIR = '<All>') AND (:P1_FLM = S.FLM OR :P1_FLM = '<All>') AND (:P1_REP = S.REP OR :P1_REP = '<All>'))
 GROUP BY Category
 ORDER BY CASE Category
 WHEN 'Rejected' THEN 1
@@ -40,16 +42,23 @@ Leads with Interactions
 SELECT 
 count(distinct REGISTRY_ID) as Value,
 'Leads with interactions' as Category
-FROM catalincostin.fy20_interactions WHERE LEAD_CAMPAIGN_CODE = :P3_CODE
+FROM catalincostin.fy20_interactions I 
+LEFT JOIN FLM_SALES S on S.REP = I.EMAIL_ADDRESS
+WHERE LEAD_CAMPAIGN_CODE = :P3_CODE AND (:P1_VP = S.VP OR :P1_VP = '<All>') AND (:P1_DIR = S.DIR OR :P1_DIR = '<All>') AND (:P1_FLM = S.FLM OR :P1_FLM = '<All>') AND (:P1_REP = S.REP OR :P1_REP = '<All>')
 UNION
 SELECT (
 	(SELECT 
 	count(distinct lead_ID)
-	FROM catalincostin.fy20_leads where CAMPAIGN_SOURCE_CODE = :P3_CODE AND RETIRE_REASON NOT LIKE 'Z-ADMIN: Admin Only%')
+	FROM catalincostin.fy20_leads lead
+    LEFT JOIN FLM_SALES S on S.REP = lead.TERRITORY_OWNER_E_MAIL
+    WHERE lead.CAMPAIGN_SOURCE_CODE = :P3_CODE AND RETIRE_REASON NOT LIKE 'Z-ADMIN: Admin Only%' 
+    AND (:P1_VP = S.VP OR :P1_VP = '<All>') AND (:P1_DIR = S.DIR OR :P1_DIR = '<All>') AND (:P1_FLM = S.FLM OR :P1_FLM = '<All>') AND (:P1_REP = S.REP OR :P1_REP = '<All>'))
 		-
 	(SELECT 
 	count(distinct REGISTRY_ID)
-	FROM catalincostin.fy20_interactions WHERE LEAD_CAMPAIGN_CODE = :P3_CODE )
+	FROM catalincostin.fy20_interactions  I 
+    LEFT JOIN FLM_SALES S on S.REP = I.EMAIL_ADDRESS
+    WHERE LEAD_CAMPAIGN_CODE = :P3_CODE AND (:P1_VP = S.VP OR :P1_VP = '<All>') AND (:P1_DIR = S.DIR OR :P1_DIR = '<All>') AND (:P1_FLM = S.FLM OR :P1_FLM = '<All>') AND (:P1_REP = S.REP OR :P1_REP = '<All>'))
 ) as Value,
 'Leads without interactions' as Category
 FROM dual 
@@ -67,7 +76,9 @@ SELECT
 RETIRE_REASON as Category,
 COUNT(RETIRE_REASON) as Value
 FROM catalincostin.FY20_LEADS lead
-WHERE lead.CAMPAIGN_SOURCE_CODE = :P3_CODE AND STATUS = 'RETIRED' AND RETIRE_REASON NOT LIKE 'Z-ADMIN: Admin Only%'
+LEFT JOIN FLM_SALES S on S.REP = lead.TERRITORY_OWNER_E_MAIL
+WHERE lead.CAMPAIGN_SOURCE_CODE = :P3_CODE AND RETIRE_REASON NOT LIKE 'Z-ADMIN: Admin Only%' 
+AND (:P1_VP = S.VP OR :P1_VP = '<All>') AND (:P1_DIR = S.DIR OR :P1_DIR = '<All>') AND (:P1_FLM = S.FLM OR :P1_FLM = '<All>') AND (:P1_REP = S.REP OR :P1_REP = '<All>')
 GROUP BY RETIRE_REASON 
 ORDER BY Value DESC 
 
